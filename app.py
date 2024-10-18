29e3e9a2b52edad753f41620cfb23f7f8bcf7481
nicoboga4075 <nicolas.bogalheiro@gmail.com>
2024-10-18 21:32:39 +0200

    Update app for required_packages

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-"""
-This module runs the FastAPI application for the NPICalculator project.
-
-It uses Uvicorn as the ASGI server to serve the application on
-host 0.0.0.0 and port 8000.
+""" This module runs the FastAPI application for the NPICalculator project.
+It uses Uvicorn as the ASGI server to serve the application.
 """
 import subprocess
 import sys
-
-# Attempts to import Uvicorn
-try:
-    import uvicorn
-except ImportError:
-    print("Uvicorn not found. Installing...")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "uvicorn"])
-    import uvicorn  # Re-imports after installation
 from NPICalculator.controller import app # Gets FastAPI object from the controller
 
+def ensure_packages_installed(required_packages):
+    """ Ensures that the specified packages are installed. """
+    for package in required_packages:
+        try:
+            globals()[package] = __import__(package)  # Import the module dynamically
+        except ImportError:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+            globals()[package] = __import__(package)  # Re-imports after installation
+
+ensure_packages_installed(["uvicorn", "fastapi", "sqlalchemy", "pandas"])
+
 if __name__ == "__main__":
+    import uvicorn
     # Launches Uvicorn server with app on port 8000 for global host
     uvicorn.run(app, host = "0.0.0.0", port = 8000)
