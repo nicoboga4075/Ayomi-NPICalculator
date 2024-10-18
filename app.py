2984a76655c7171638e3492ff771d66a8056089b
nicoboga4075 <nicolas.bogalheiro@gmail.com>
2024-10-18 21:20:38 +0200

    Test import out of virtual env.

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ This module runs the FastAPI application for the NPICalculator project.
 It uses Uvicorn as the ASGI server to serve the application on
 host 0.0.0.0 and port 8000.
 """
+import subprocess
+import sys
 
-import uvicorn
+# Attempts to import Uvicorn
+try:
+    import uvicorn
+except ImportError:
+    print("Uvicorn not found. Installing...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "uvicorn"])
+    import uvicorn  # Re-imports after installation
 from NPICalculator.controller import app # Gets FastAPI object from the controller
 
 if __name__ == "__main__":
